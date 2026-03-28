a33702f0e72e270f3c90d70b44a4e278c8bfecca
RobertP2705 <prevostrobert309@gmail.com>
2026-03-28 00:22:36 -0500

    adding replica and trader headers

--- sequencer.cpp
+++ sequencer.cpp
@@ -1,3 +1,9 @@
-Class Sequencer {
-    
-}
+class Sequencer {
+    private: 
+        volatile int sequenceNumber;
+    public:
+        Sequencer(){
+            int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+        };
+};
