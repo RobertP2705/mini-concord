266827c3caca3a40113e0c751bd19998a29e1893
RobertP2705 <136493854+RobertP2705@users.noreply.github.com>
2026-03-27 23:52:07 -0500

    setting up

---
+++ sequencer.h
@@ -0,0 +1,13 @@
+#ifndef SEQUENCER_H
+#define SEQUENCER_H
+
+#include <sys/socket.h>
+
+class Sequencer {
+    private: 
+        volatile int sequenceNumber;
+    public:
+        Sequencer();
+};
+
+#endif 
